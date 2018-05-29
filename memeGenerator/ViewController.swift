c0070a462db4b24733aadbd2ede19145a8398279
William Castellano <castellkid@aol.com>
2018-05-29 16:09:30 -0400

    Textfield properties not set up to look like meme type font

--- memeGenerator/ViewController.swift
+++ memeGenerator/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+  
   @IBOutlet var imageView: UIImageView!
   @IBOutlet var cameraButton: UIBarButtonItem!
+  @IBOutlet var topTextField: UITextField!
+  @IBOutlet var bottomTextField: UITextField!
+  
+  let memeTextAttributes: [String: Any] = [
+    NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
+    NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
+    NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+    NSAttributedStringKey.strokeWidth.rawValue: 3.0
+  ]
+  
+  // MARK: - Load Functions
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    topTextField.text = "TOP"
+    bottomTextField.text = "BOTTOM"
+    
+    topTextField.textAlignment = .center
+    bottomTextField.textAlignment = .center
+    
+    topTextField.delegate = self
+    bottomTextField.delegate = self
+    
+    topTextField.defaultTextAttributes = memeTextAttributes
+    bottomTextField.defaultTextAttributes = memeTextAttributes
   }
   
   override func viewWillAppear(_ animated: Bool) {
     cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
   }
+  
+  // MARK: - Image Picker
   
   @IBAction func pickImageFromPhotos(_ sender: Any) {
     let imagePicker = UIImagePickerController()
@@ -40,6 +65,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
       imageView.image = image
       dismiss(animated: true, completion: nil)
     }
+  }
+  
+  // MARK: - TextField Functions
+  
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    textField.text = ""
+  }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return true
   }
 }
 
